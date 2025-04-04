324ec6e4d1b8e572959f480aba9734bf23f3bada
saqlinezaman <saqlinemoaj@gmail.com>
2025-04-04 15:40:02 +0600

    all section done

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <script setup>
 import HeaderSectionComponent from './components/HeaderSectionComponent.vue';
 import HeroSectionComponent from './components/HeroSectionComponent.vue';
+import CategorySectionComponents from './components/CategorySectionComponents.vue';
+import NewCollectionComponents from './components/NewCollectionComponents.vue';
+import ProductSectionComponenets from './components/ProductSectionComponenets.vue';
+import ContactSectionComponents from './components/ContactSectionComponents.vue';
+import FooterSectionComponents from './components/FooterSectionComponents.vue';
 </script>
 
 <template>
@@ -12,268 +17,26 @@ import HeroSectionComponent from './components/HeroSectionComponent.vue';
     <HeroSectionComponent/>
   <!-- Hero section end  -->
 
- 
+  <!-- Category section -->
+    <CategorySectionComponents/>
 
-  <section class="p-4 py-20">
-    <div class="w-full max-w-7xl mx-auto">
-      <div class="flex flex-col lg:flex-row gap-4">
-        <div
-          class="flex w-full rounded-xl p-8 min-h-[400px] md:min-h-[600px] lg:min-h-[800px] bg-cover bg-center border"
-          style="background-image:url(https://images.unsplash.com/photo-1517841905240-472988babdf9?q=80&w=2459&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
-          <div class="mt-auto flex flex-col md:flex-row gap-4 w-full">
-            <a href=""
-              class="bg-white hover:bg-black hover:text-white p-2 text-xl rounded-full w-full flex gap-4 justify-between items-center">
-              <span class="pl-2">Learn more</span>
-              <i
-                class="flex items-center justify-center bi bi-arrow-down bg-black text-white rounded-full p-1 w-[40px] h-[40px] text-center"></i>
-            </a>
-            <a href=""
-              class="bg-white hover:bg-black hover:text-white p-2 text-xl rounded-full w-full flex gap-4 justify-between items-center">
-              <span class="pl-2">Contact us</span>
-              <i
-                class="flex items-center justify-center bi bi-envelope bg-black text-white rounded-full p-1 w-[40px] h-[40px] text-center"></i>
-            </a>
-          </div>
-        </div>
-        <div class="w-full flex flex-col gap-4">
-          <div class="w-full h-full rounded-xl p-8 bg-gradient-to-tr from-gray-200 to-gray-100  shadow-xl">
-            <h1 class="text-2xl lg:text-4xl mb-auto pb-10">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            </h1>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque, neque iusto. Aperiam animi, facere
-              numquam corrupti fugit non laboriosam excepturi quaerat a mollitia ut.
-            </p>
-          </div>
-          <div class="h-full flex flex-col md:flex-row gap-4">
-            <a href=""
-              class="min-h-[150px] flex w-full overflow-hidden rounded-xl bg-gray-300 bg-cover bg-center border"
-              style="background-image:url(https://images.unsplash.com/photo-1490481651871-ab68de25d43d?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
-              <p class="text-white text-lg font-bold mt-auto w-full p-2 bg-black bg-opacity-70">
-                #NewCollection
-              </p>
-            </a>
-            <a href=""
-              class="min-h-[150px] flex w-full overflow-hidden rounded-xl bg-gray-300 bg-cover bg-center border"
-              style="background-image:url(https://images.unsplash.com/photo-1529374255404-311a2a4f1fd9?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)">
-              <p class="text-white text-lg font-bold mt-auto w-full p-2 bg-black bg-opacity-70">
-                #ContentCreator
-              </p>
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-  <section class="p-4 py-20">
-    <div class="w-full max-w-7xl mx-auto">
-      <div class="pb-8 flex justify-between items-center gap-8">
-        <h2 class="text-4xl lg:text-5xl pb-4">
-          Our collections
-        </h2>
-        <div class="hidden md:flex gap-6 text-2xl">
-          <a href="">
-            <i
-              class="flex items-center justify-center text-black border border-black rounded-full p-2 bi bi-arrow-left"></i>
-          </a>
-          <a href="">
-            <i
-              class="flex items-center justify-center text-black border border-black rounded-full p-2 bi bi-arrow-right"></i>
-          </a>
-        </div>
-      </div>
+  <!-- Category section end -->
 
-      <div class="flex gap-4">
-        <a href="" class="w-full flex flex-col gap-2">
-          <div class="max-h-[300px] md:max-h-[500px] overflow-hidden rounded-xl shadow-xl relative">
-            <i
-              class="flex items-center justify-center text-white rounded-full bg-black p-2 bi bi-arrow-up-right absolute top-4 right-4"></i>
-            <img class="w-full"
-              src="https://images.unsplash.com/photo-1515886657613-9f3515b0c78f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZmFzaGlvbnxlbnwwfHwwfHx8MA%3D%3D"
-              alt="">
-          </div>
-          <h3 class="text-2xl pt-4">New collection</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. et dolor y etlerim dolor gils de nada!
-          </p>
-        </a>
-        <a href="" class="w-full flex flex-col gap-2">
-          <div class="max-h-[300px] md:max-h-[500px] overflow-hidden rounded-xl shadow-xl relative">
-            <i
-              class="flex items-center justify-center text-white rounded-full bg-black p-2 bi bi-arrow-up-right absolute top-4 right-4"></i>
-            <img class="w-full"
-              src="https://images.unsplash.com/photo-1529139574466-a303027c1d8b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZmFzaGlvbnxlbnwwfHwwfHx8MA%3D%3D"
-              alt="">
-          </div>
-          <h3 class="text-2xl pt-4">Sales</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. et dolor y etlerim dolor gils de nada!
-          </p>
-        </a>
-        <a href="" class="w-full hidden md:flex flex-col gap-2">
-          <div class="max-h-[300px] md:max-h-[500px] overflow-hidden rounded-xl shadow-xl relative">
-            <i
-              class="flex items-center justify-center text-white rounded-full bg-black p-2 bi bi-arrow-up-right absolute top-4 right-4"></i>
-            <img class="w-full"
-              src="https://images.unsplash.com/photo-1485968579580-b6d095142e6e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzF8fGZhc2hpb258ZW58MHx8MHx8fDA%3D"
-              alt="">
-          </div>
-          <h3 class="text-2xl pt-4">Classics</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. et dolor y etlerim dolor gils de nada!
-          </p>
-        </a>
-      </div>
-    </div>
-  </section>
-  <section class="p-4 py-20">
-    <div class="w-full max-w-7xl mx-auto">
-      <div class="flex flex-col gap-8">
-        <div class="flex flex-col lg:flex-row justify-between lg:items-center gap-4">
-          <h2 class="text-3xl lg:text-5xl pb-4">
-            Featured products
-          </h2>
-          <div class="flex gap-4 text-xl">
-            <a href="" class="p-2 px-4 border border-black hover:bg-black hover:text-white rounded-full">
-              All
-            </a>
-            <a href="" class="p-2 px-4 border border-black bg-black text-white rounded-full">
-              New arrival
-            </a>
-            <a href="" class="p-2 px-4 border border-black hover:bg-black hover:text-white rounded-full">
-              Best seller
-            </a>
-          </div>
-        </div>
+  <!-- New collection section -->
+   <NewCollectionComponents/>
+  <!-- New collection section end -->
 
-        <div class="flex gap-4">
-          <a href="" class="w-full flex flex-col gap-2">
-            <div class="max-h-[300px] md:max-h-[400px] overflow-hidden rounded-xl shadow-xl relative">
-              <i
-                class="flex items-center justify-center text-white rounded-full bg-black p-2 bi bi-arrow-up-right absolute top-4 right-4"></i>
-              <img class="w-full"
-                src="https://images.unsplash.com/photo-1566206091558-7f218b696731?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="">
-            </div>
-            <h3 class="text-2xl pt-4 flex flex-col lg:flex-row lg:items-center">White dress <span
-                class="lg:ml-auto text-yellow-700 font-bold">240$</span></h3>
-            <div class="flex gap-2">
-              <div class="rounded-full w-[20px] h-[20px] bg-white border shadow"></div>
-              <div class="rounded-full w-[20px] h-[20px] bg-gray-200 border shadow"></div>
-            </div>
-          </a>
-          <a href="" class="w-full flex flex-col gap-2">
-            <div class="max-h-[300px] md:max-h-[400px] overflow-hidden rounded-xl shadow-xl relative">
-              <i
-                class="flex items-center justify-center text-white rounded-full bg-black p-2 bi bi-arrow-up-right absolute top-4 right-4"></i>
-              <img class="w-full"
-                src="https://images.unsplash.com/photo-1608748010899-18f300247112?q=80&w=3464&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="">
-            </div>
-            <h3 class="text-2xl pt-4 flex flex-col lg:flex-row lg:items-center">Brown dress <span
-                class="lg:ml-auto text-yellow-700 font-bold">629.99$</span></h3>
-            <div class="flex gap-2">
-              <div class="rounded-full w-[20px] h-[20px] bg-amber-800 border shadow"></div>
-              <div class="rounded-full w-[20px] h-[20px] bg-orange-200 border shadow"></div>
-            </div>
-          </a>
-          <a href="" class="w-full hidden md:flex flex-col gap-2">
-            <div class="max-h-[300px] md:max-h-[400px] overflow-hidden rounded-xl shadow-xl relative">
-              <i
-                class="flex items-center justify-center text-white rounded-full bg-black p-2 bi bi-arrow-up-right absolute top-4 right-4"></i>
-              <img class="w-full"
-                src="https://images.unsplash.com/photo-1625204614387-6509254d5b02?q=80&w=3024&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="">
-            </div>
-            <h3 class="text-2xl pt-4 flex flex-col lg:flex-row lg:items-center">Lorem dress <span
-                class="lg:ml-auto text-yellow-700 font-bold">742.30$</span></h3>
-            <div class="flex gap-2">
-              <div class="rounded-full w-[20px] h-[20px] bg-pink-600 border shadow"></div>
-              <div class="rounded-full w-[20px] h-[20px] bg-lime-500 border shadow"></div>
-            </div>
-          </a>
-        </div>
+  <!-- product section -->
+   <ProductSectionComponenets/>
+  <!-- product section end -->
 
-        <a href="" class="p-2 px-4 border border-black hover:bg-black hover:text-white rounded-full mx-auto text-xl">
-          See all products <i class="bi bi-arrow-up-right"></i>
-        </a>
-      </div>
-    </div>
-  </section>
-  <section class="p-4 py-20">
-    <div class="mx-auto max-w-7xl w-full">
-      <div class="grid lg:grid-cols-2 grid-cols-1 items-center">
-        <div class="lg:mb-0 mb-10">
-          <div class="group w-full h-full">
-            <div class="relative h-full">
-              <img
-                src="https://images.unsplash.com/photo-1516762689617-e1cffcef479d?q=80&w=3711&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="ContactUs tailwind section" class="w-full h-full lg:rounded-l-2xl rounded-2xl bg-blend-multiply" />
-              <h1 class="font-manrope text-black text-4xl font-bold leading-10 absolute top-11 left-11">Contact us</h1>
-              <div class="absolute bottom-0 w-full lg:p-11 p-5">
-                <div class="bg-white rounded-lg p-6 block">
-                  <a href="#" class="flex items-center mb-6">
-                    <i class="bi bi-telephone"></i>
-                    <h5 class="underline text-black text-base font-normal leading-6 ml-5">123-123-123</h5>
-                  </a>
-                  <a href="mailto:pro@lingare.com" class="flex items-center mb-6">
-                    <i class="bi bi-envelope"></i>
-                    <h5 class="underline text-black text-base font-normal leading-6 ml-5">pro@email.com</h5>
-                  </a>
-                  <a href="#" class="flex items-center">
-                    <i class="bi bi-geo-alt"></i>
-                    <h5 class="underline text-black text-base font-normal leading-6 ml-5">424 Yallala Avenue,
-                      Meadowville, WA
-                      76543</h5>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+  <!-- contact section -->
+  <ContactSectionComponents/>
+   <!-- contact section end -->
 
-        <div class="p-5 lg:p-11 lg:rounded-r-2xl rounded-2xl">
-          <h2 class="font-manrope text-4xl font-semibold leading-10 mb-11">Send Us A Message</h2>
-          <input type="text"
-            class="w-full h-12 text-gray-600 placeholder-gray-400  shadow-sm bg-transparent text-lg font-normal leading-7 border border-black focus:outline-none p-4 mb-10"
-            placeholder="Name">
-          <input type="text"
-            class="w-full h-12 text-gray-600 placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal leading-7 border border-black focus:outline-none p-4 mb-10"
-            placeholder="Email">
-          <input type="text"
-            class="w-full h-12 text-gray-600 placeholder-gray-400 shadow-sm bg-transparent text-lg font-normal leading-7 border border-black focus:outline-none p-4 mb-10"
-            placeholder="Phone">
-          <textarea
-            class="max-h-[200px] w-full text-gray-600 placeholder-gray-400 bg-transparent text-lg shadow-sm font-normal leading-7 border border-black focus:outline-none p-4 mb-10"
-            placeholder="Message"></textarea>
-          <button
-            class="w-full h-12 text-white text-base font-semibold leading-6 rounded-full transition-all duration-700 bg-black shadow-sm">Send</button>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <footer class="p-4 pb-10 pt-20">
-    <div class="gap-8 flex justify-between mx-auto max-w-7xl w-full">
-      <div class="flex justify-center space-x-6">
-        <span class="inline-flex justify-center w-full gap-3 lg:ml-auto md:justify-start md:w-auto">
-          <a class="size-6 transition fill-black hover:text-blue-500">
-            <span class="sr-only"> twitter </span>
-            <i class="bi bi-twitter-x"></i>
-          </a>
-          <a class="size-6 transition fill-black hover:text-blue-500">
-            <span class="sr-only"> Instagram </span>
-            <i class="bi bi-instagram"></i>
-          </a>
-        </span>
-      </div>
-      <div>
-        <span class="text-sm font-medium text-gray-500">
-          Copyright © <span>2024</span>
-        </span>
-      </div>
-    </div>
-  </footer>
+  <!-- foter section -->
+   <FooterSectionComponents/>
+   <!-- foter section end -->
 </template>
 
 <style scoped>
